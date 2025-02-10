0b704967dda653492cef093d1780ce70d6224098
Ahmed Wagdy <ahmedwa122@gmail.com>
2025-02-10 08:26:50 +0200

    add logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,5 +14,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3030);
+
+  const logger = new Logger();
+  logger.debug(
+    `This application is runnning on: ${await app.getUrl()}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  private readonly logger = new Logger(PrismaService.name, { timestamp: true });
+
+  doSomething() {
+    this.logger.log('Doing something with timestamp here ->');
+  }
+}
